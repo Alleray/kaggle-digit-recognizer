da5ff013bda5e3c4daaf432ab32e5e059c858899
Aleksandr Zolotarev <al.m.zolotarev@gmail.com>
2018-08-05 18:01:24 +0300

    change model.py

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from keras.models import Model, load_model
 from keras.layers import Input, Dense, Dropout
 from keras.utils import to_categorical
+from keras.optimizers import Adam
 from keras.backend import argmax
 import os
 
@@ -18,7 +19,7 @@ if not os.path.isfile('model.h5'):
 
     Y = train_data[:, 0]  #labels (0-9), 1st column of the train_data
     X = train_data[:, 1:] #pixel values, columns from 1 to end
-    X /= 255 #normalizing parameters
+    X /= 255 #normalizing train_data
 
     #splitting train_data to train and dev sets ~70-30%
     X_train = X[:30000]
@@ -53,27 +54,28 @@ if not os.path.isfile('model.h5'):
 
     print('Initiating model...')
     model = model(X_train.shape[1:])
-    model.compile(optimizer='Adam', loss='mean_squared_error', metrics=['accuracy'])
+    adam = Adam(lr=0.0001, decay=0.00001)
+    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 
     print('Fitting model...')
-    model.fit(x=X_train, y=Y_train_oh, epochs=40, batch_size=512)
+    model.fit(x=X_train, y=Y_train_oh, epochs=100, batch_size=1000)
 
     print('Fitting is complete.')
     print('Saving trained model...')
     model.save('model.h5')
 
-    """
     #Uncomment this to evaluate model on the dev set
     print('Evaluating dev set...')
     predictions = model.evaluate(x=X_dev, y=Y_dev_oh)
 
     print()
     print('Loss =', round(predictions[0], 7))
-    print('Dev set accuracy:', round(predictions[1], 3)
-    """
+    print('Dev set accuracy:', round(predictions[1], 3))
+
 else:
     print('Loading model...')
     model = load_model('model.h5')
+
 
 print()
 print('Loading test set...')
@@ -81,15 +83,14 @@ test_data = np.genfromtxt('datasets/test.csv', delimiter=',', skip_header=1)
 test_data /= 255 #normalizing test data
 
 print('Predicting labels...')
-predictions = model.predict(x=test_data, verbose=1, batch_size=512)
+predictions = model.predict(x=test_data, verbose=1, batch_size=1000)
 predictions = np.argmax(predictions, axis=-1)
 #print(predictions[0:10])
 
-labels = predictions.astype(int) #getting rid of 2.000000000000000000e+00
 imageid = np.arange(1, len(predictions)+1) #creating id axis
 
 #adding new axis to be able to concatenate correctly
-labels = labels[np.newaxis]
+labels = predictions[np.newaxis]
 imageid = imageid[np.newaxis]
 
 print(labels.shape, imageid.shape)
